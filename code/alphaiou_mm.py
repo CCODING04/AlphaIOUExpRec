27eb03438787d386e8e7dc617e4f210c0047034e
ccoding <ccoding04@gmail.com>
2022-07-01 12:31:54 +0800

    add test code and original code of AlphaIoU

---
+++ code/alphaiou_mm.py
@@ -0,0 +1,69 @@
+import torch
+
+def alphaiou_loss(pred, target, alpha=3, eps=1e-9, mode='iou'):
+    # iou mode
+    mode = mode.lower()
+    assert mode in ('iou', 'ciou', 'giou', 'diou')
+
+    b1_x1, b1_y1 = pred[:, 0], pred[:, 1]
+    b1_x2, b1_y2 = pred[:, 2], pred[:, 3]
+    b2_x1, b2_y1 = target[:, 0], target[:, 1]
+    b2_x2, b2_y2 = target[:, 2], target[:, 3]
+
+    # overlap
+    overlap = (torch.min(b1_x2, b2_x2) - torch.max(b1_x1, b2_x1)).clamp(0) *\
+              (torch.min(b1_y2, b2_y2) - torch.max(b1_y1, b2_y1)).clamp(0)
+
+    # union
+
+    w1, h1 = b1_x2 - b1_x1, b1_y2 - b1_y1 + eps
+    w2, h2 = b2_x2 - b2_x1, b2_y2 - b2_y1 + eps
+    union = w1 * h1 + w2 * h2 - overlap + eps
+
+    # change conventional iou to alpha pow
+    ious = torch.pow(overlap / union + eps, alpha)
+
+    # calculate alpha-iou according mode
+    if mode == 'iou':
+        loss = 1 - ious
+        return loss
+
+    # enclose area
+    enclose_x1y1 = torch.min(pred[:, :2], target[:, :2])
+    enclose_x2y2 = torch.max(pred[:, 2:], target[:, 2:])
+    enclose_wh = (enclose_x2y2 - enclose_x1y1).clamp(min=0)
+
+    cw = enclose_wh[:, 0]
+    ch = enclose_wh[:, 1]
+
+    if mode == 'giou':
+        c_area = torch.max(cw * ch + eps, union)
+        gious = ious - torch.pow((c_area - union) / c_area + eps, alpha)
+        loss = 1 - gious
+        return loss
+
+    c2 = (cw**2 + ch**2)**alpha + eps
+
+    left = ((b2_x1 + b2_x2) - (b1_x1 + b1_x2))**2 / 4
+    right = ((b2_y1 + b2_y2) - (b1_y1 + b1_y2))**2 / 4
+    rho2 = (left + right)**alpha
+
+    # DIoU
+    if mode == 'diou':
+        dious = ious - rho2 / c2
+        loss = 1 - dious
+        return loss
+    else:
+        w1, h1 = b1_x2 - b1_x1, b1_y2 - b1_y1 + eps
+        w2, h2 = b2_x2 - b2_x1, b2_y2 - b2_y1 + eps
+
+        factor = 4 / math.pi**2
+        v = factor * torch.pow(torch.atan(w2 / h2) - torch.atan(w1 / h1), 2)
+
+        with torch.no_grad():
+            alpha_ciou = (ious > 0.5).float() * v / (1 - ious + v)
+
+        # CIoU
+        cious = ious - (rho2 / c2 + torch.pow(alpha_ciou * v, alpha))
+        loss = 1 - cious
+        return loss
---
+++ code/test.py
@@ -0,0 +1,84 @@
+import cv2
+import random
+import numpy as np
+
+import torch
+from alphaiou_o import bbox_alpha_iou
+
+
+def gen_bbox(seed=None):
+    """
+    Random generate bounding boxes for IOU caculation
+    Args:
+        seed[int]: random seed
+    Returns:
+        bbox1[np.ndarray]: x1y1x2y2
+        bbox2[np.ndarray]: x1y1x2y2
+    """
+    if seed is not None:
+        random.seed(seed)
+        np.random.seed(seed)
+    # step1 : random gen bbox1
+    xcyc = np.random.randint(0, 300, size=(2,))
+    wh = np.random.randint(100, 320, size=(2,))
+    bbox1 = [ 
+                xcyc[0] - wh[0]//2,
+                xcyc[1] - wh[1]//2,
+                xcyc[0] + wh[0]//2,
+                xcyc[1] + wh[1]//2
+            ]
+    bbox1 = [x.item() for x in bbox1]
+    bbox1_arr = np.array(bbox1, dtype='int')
+
+    # step2 : choose which point of bbox2 land in bbox1
+    # points orderd by clockwise and 0 represent lt point of bbox1
+    which_point = random.randint(0,3)
+    xn = random.randint(bbox1[0], bbox1[2])
+    yn = random.randint(bbox1[1], bbox1[3])
+    w, h = [random.randint(32, 320) for _ in range(2)]
+    if which_point == 0:
+        bbox2 = [xn, yn, xn + w, yn + h]
+    elif which_point == 1:
+        bbox2 = [xn - w, yn, xn, yn + h]
+    elif which_point == 2:
+        bbox2 = [xn - w, yn - h, xn, yn]
+    elif which_point == 3:
+        bbox2 = [xn, yn -h, xn + w, yn]
+    bbox2_arr = np.array(bbox2, dtype='int')
+    
+    return bbox1_arr, bbox2_arr
+
+
+def vis(bbox1, bbox2):
+    min_v = min(bbox1.min().item(), bbox2.min().item()) - 10
+    max_v = max(bbox1.max().item(), bbox2.max().item()) + 10
+    bbox1 = bbox1 - min_v
+    bbox2 = bbox2 - min_v
+    hw = max_v - min_v
+    canvas = np.ones((hw, hw, 3), dtype=np.uint8)*255
+
+    cv2.rectangle(canvas, tuple(bbox1[:2]), tuple(bbox1[2:]), (0, 255, 0), -1)
+    cv2.rectangle(canvas, tuple(bbox2[:2]), tuple(bbox2[2:]), (0, 0, 255), -1)
+    bbox3 = [
+        max(bbox1[0], bbox2[0]),
+        max(bbox1[1], bbox2[1]),
+        min(bbox1[2], bbox2[2]),
+        min(bbox1[3], bbox2[3])
+        ]
+    bbox3 = np.array([x.item() for x in bbox3])
+    cv2.rectangle(canvas, tuple(bbox3[:2]), tuple(bbox3[2:]), (0, 128, 128), -1)
+
+    return canvas
+
+
+def main():
+    bbox1, bbox2 = gen_bbox()
+    canvas = vis(bbox1, bbox2)
+    cv2.imwrite('vis.png', canvas)
+    bbox1 = torch.from_numpy(bbox1)
+    bbox2 = torch.from_numpy(bbox2)
+    aiou = bbox_alpha_iou(bbox1, bbox2, x1y1x2y2=True, alpha=3)
+    print(aiou)
+
+if __name__ == "__main__":
+    main()
